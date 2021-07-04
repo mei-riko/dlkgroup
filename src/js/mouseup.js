46e90539a72c2fd0702774b760d30f3145495434
mei-riko <dobrovolska.marina@gmail.com>
2021-07-04 14:54:28 +0300

    check

---
+++ src/js/mouseup.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+$(document).on('mouseup', function (e){
+    // if (!navbarActive.is(e.target) // клик был не по блоку
+    //       && navbarActive.has(e.target).length === 0 // и не по его дочерним элементам
+    //       && !navbarItem.is(e.target)
+    //       && $('body').hasClass('open-navbar')
+    // ) {
+    //     closeNavbarOnClick();
+    // }
+});
